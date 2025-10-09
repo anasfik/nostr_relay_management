bc1e85db1c4d607f3580d3adecbb4d5b43643d82
Anas Fikhi <work@gwhyyy.com>
2025-10-09 15:34:53 +0100

    [ Add ] add more endpoint handling for nip86 endpoints, for the example go relay based on khatru

--- example/go_relay/main.go
+++ example/go_relay/main.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-
+	"github.com/nbd-wtf/go-nostr/nip86"
 	"github.com/fiatjaf/eventstore/sqlite3"
 	"github.com/fiatjaf/khatru"
 
@@ -34,6 +34,110 @@ func main() {
 		return []int{0, 3, 10000, 10001, 10002, 10003, 10004, 10008, 10009, 10016, 30000}, nil
 	}
 
+	relay.ManagementAPI.ListAllowedPubKeys = func(ctx context.Context) ([]nip86.PubKeyReason, error) {
+		// Check if context is cancelled
+		if ctx.Err() != nil {
+			return nil, ctx.Err()
+		}
+
+		return []nip86.PubKeyReason{
+			{
+				PubKey: "pubkey1",
+				Reason: "Spam",
+			},
+			{
+				PubKey: "pubkey2",
+				Reason: "Harassment",
+			},
+		}, nil
+	}
+
+	relay.ManagementAPI.ListAllowedEvents = func(ctx context.Context) ([]nip86.IDReason, error) {
+
+		idsAndReasonEntrie := []nip86.IDReason{
+			{
+				ID:     "eventId3",
+				Reason: "Verified content",
+			},
+			{
+				ID:     "eventId4",
+				Reason: "Trusted source",
+			},
+
+		}
+
+		return idsAndReasonEntrie, nil
+
+
+	}
+
+
+	relay.ManagementAPI.ListBannedEvents = func(ctx context.Context) ([]nip86.IDReason, error) {
+
+
+		idsAndReasonEntrie := []nip86.IDReason{
+			{
+				ID:     "eventId1",
+				Reason: "Inappropriate content",
+			},
+			{
+				ID:     "eventId2",
+				Reason: "Spam",
+			},
+
+		}
+
+
+		return idsAndReasonEntrie, nil
+	}
+
+
+
+	relay.ManagementAPI.ListDisAllowedKinds = func(ctx context.Context) ([]int, error) {
+
+		kinds := []int{1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+
+		return kinds, nil
+	}
+
+	relay.ManagementAPI.ListBannedPubKeys = func(ctx context.Context) ([]nip86.PubKeyReason, error) {
+
+		entries := []nip86.PubKeyReason{
+			{
+				PubKey: "pubkey1",
+				Reason: "maybe spam",
+			},
+		}
+
+		return entries, nil
+	}
+
+	relay.ManagementAPI.ListBlockedIPs = func(ctx context.Context) ([]nip86.IPReason, error) {
+
+		entries := []nip86.IPReason{
+			{
+				IP:     "102.2.5.4",
+				Reason: "suspicious activity",
+
+			},
+		}
+
+		return entries, nil
+	}
+
+
+	relay.ManagementAPI.ListEventsNeedingModeration = func(ctx context.Context) ([]nip86.IDReason, error) {
+
+		entries := []nip86.IDReason{
+			{
+				ID:     "eventId5",
+				Reason: "contain sensitive content to be reviewed (detected by AI)",
+			},
+		}
+
+		return entries, nil
+
+	}
 
 	fmt.Printf("Listening on :3334\n")
 	http.ListenAndServe(":3334", relay)
